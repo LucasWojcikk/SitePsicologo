4b93929d0f8f639cd69b3c07ae18a3be510f820b
Lucas Wojcik <131882265+LucasWojcikk@users.noreply.github.com>
2024-05-20 18:46:36 -0300

    Squashed commit of the following:
    commit 6fe3a73555a75ca83aa4834ea7a93faa6e6a0e12
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 17:25:21 2024 -0300
    
        Botão instagram
    
    commit dd10d8b5e491b1ec5edabad586f192403b425cb2
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 17:21:20 2024 -0300
    
        Botão redes sociais
    
    commit 25b712314c2f69eba2f68387506374069a8a624f
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 17:09:50 2024 -0300
    
        Alinhamento Jumbotrom
    
    commit 8433f04dbf9dc27773398bf5b51af999bdfb4930
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 14:20:53 2024 -0300
    
        Atualização login
    
    commit 641099b2937d45dbe5577136c674bd613f33dada
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 14:14:47 2024 -0300
    
        update
    
    commit 00d11ee844c6f69a9f324039f37c3fbefbbce752
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 13:19:39 2024 -0300
    
        Update index.html
    
    commit 41aa3c640dc09aa3d68d640860d2f4a02632def7
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 13:19:11 2024 -0300
    
        Update index.html
    
    commit 03d3da34ef03d274160a32056d562162df186c8b
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 13:17:36 2024 -0300
    
        Update pagina inicial
    
    commit 6966ad1020dae71d80ea2a5ce9cf3d34dcc472ec
    Author: Letícia Brudeck <leticiabrudeck_@hotmail.com>
    Date:   Mon May 20 13:15:26 2024 -0300
    
        logo

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -5,95 +5,71 @@ function getResponseApi() {
         })
         .then(function(data) {
             data.forEach(function(post) {
-                addPost(post)
-                // console.log(post);
+                addPost(post);
             });
         });
 }
 
-// function addPost(post) {
-//     console.log(post["titulo"])
-
-// }
-
 function addPost(post) {
-    // Seleciona a div onde as postagens serão inseridas
-    const postagensContainer = document.getElementById("postagens");
+    const divPost = document.getElementById("postagens");
 
-    // Cria a estrutura HTML para uma nova postagem com flip card
-    const postElement = document.createElement("div");
-    postElement.className = "col-md-4";
-    postElement.innerHTML = `
-        <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <p class="title">${post.titulo}</p>
-                </div>
-                <div class="flip-card-back">
-                    <p>${post.resumo}</p>
-                    <p>${post.autor}</p>
-                    <p>${post.dataPostagem}</p>
-                </div>
-            </div>
-        </div>
-    `;
-
-    // Adiciona a nova postagem ao container de postagens
-    postagensContainer.querySelector('.row').appendChild(postElement);
+    for (const key in post) {
+        const postItem = document.createElement("p");
+        postItem.textContent = `${key}: ${post[key]}`;
+        divPost.appendChild(postItem);
+    }
 }
 
-document.addEventListener('DOMContentLoaded', getResponseApi);
+function createNewPost(titulo, resumo, conteudo, dataPostagem, autor){
+    fetch("http://127.0.0.1:5500/createPost.html",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                titulo: titulo,
+                resumo: resumo,
+                conteudo: conteudo,
+                dataPostagem: dataPostagem,
+                autor: autor
+            })
+        })
+        .then(function (res) {console.log(res)}).
+    catch(function (res) {console.log(res)})
+}
 
-// function createNewPost(titulo, resumo, conteudo, dataPostagem, autor){
-//     fetch("http://127.0.0.1:5500/createPost.html",
-//     {
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         method: "POST",
-//         body: JSON.stringify({
-//             titulo: titulo,
-//             resumo: resumo,
-//             conteudo: conteudo,
-//             dataPostagem: dataPostagem,
-//             autor: autor
-//         })
-//     })
-//     .then(function (res) {console.log(res)}).
-//     catch(function (res) {console.log(res)})
-// }
+function limparForm(regTitulo, regResumo, regConteudo, regDataPostagem, regAutor) {
+    regTitulo.value = ""
+    regResumo.value = ""
+    regConteudo.value = ""
+    regDataPostagem.value = ""
+    regAutor.value = ""
+}
 
-// function limparForm(regTitulo, regResumo, regConteudo, regDataPostagem, regAutor) {
-//     regTitulo.value = ""
-//     regResumo.value = ""
-//     regConteudo.value = ""
-//     regDataPostagem.value = ""
-//     regAutor.value = ""
-// }
+document.addEventListener('DOMContentLoaded', function() {
+    const newPost = document.querySelector("form");
+    const regAutor = document.querySelector(".autor");
+    const regConteudo = document.querySelector(".conteudo");
+    const regDataPostagem = document.querySelector(".data");
+    const regResumo = document.querySelector(".resumo");
+    const regTitulo = document.querySelector(".titulo");
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const newPost = document.querySelector("form");
-//     const regAutor = document.querySelector(".autor");
-//     const regConteudo = document.querySelector(".conteudo");
-//     const regDataPostagem = document.querySelector(".data");
-//     const regResumo = document.querySelector(".resumo");
-//     const regTitulo = document.querySelector(".titulo");
+    newPost.addEventListener('submit', function (event){
+        event.preventDefault();
 
-//     newPost.addEventListener('submit', function (event){
-//         event.preventDefault();
-        
-//         createNewPost(
-//             regTitulo.value, regResumo.value, regConteudo.value, regDataPostagem.value, regAutor.value
-//         )
-//         // const dados = {
-//         //     titulo: regTitulo.value,
-//         //     resumo: regResumo.value,
-//         //     conteudo: regConteudo.value,
-//         //     dataPostagem: regDataPostagem.value,
-//         //     autor: regAutor.value,
-//         // };
-//         // console.log(dados);
-//         limparForm(regTitulo, regResumo, regConteudo, regDataPostagem, regAutor)
-//     });
-// });
+        createNewPost(
+            regTitulo.value, regResumo.value, regConteudo.value, regDataPostagem.value, regAutor.value
+        )
+        // const dados = {
+        //     titulo: regTitulo.value,
+        //     resumo: regResumo.value,
+        //     conteudo: regConteudo.value,
+        //     dataPostagem: regDataPostagem.value,
+        //     autor: regAutor.value,
+        // };
+        // console.log(dados);
+        limparForm(regTitulo, regResumo, regConteudo, regDataPostagem, regAutor)
+    });
+});
