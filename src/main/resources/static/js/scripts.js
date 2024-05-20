25b712314c2f69eba2f68387506374069a8a624f
Letícia Brudeck <leticiabrudeck_@hotmail.com>
2024-05-20 17:09:50 -0300

    Alinhamento Jumbotrom

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -22,21 +22,21 @@ function addPost(post) {
 
 function createNewPost(titulo, resumo, conteudo, dataPostagem, autor){
     fetch("http://127.0.0.1:5500/createPost.html",
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({
-            titulo: titulo,
-            resumo: resumo,
-            conteudo: conteudo,
-            dataPostagem: dataPostagem,
-            autor: autor
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                titulo: titulo,
+                resumo: resumo,
+                conteudo: conteudo,
+                dataPostagem: dataPostagem,
+                autor: autor
+            })
         })
-    })
-    .then(function (res) {console.log(res)}).
+        .then(function (res) {console.log(res)}).
     catch(function (res) {console.log(res)})
 }
 
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     newPost.addEventListener('submit', function (event){
         event.preventDefault();
-        
+
         createNewPost(
             regTitulo.value, regResumo.value, regConteudo.value, regDataPostagem.value, regAutor.value
         )
