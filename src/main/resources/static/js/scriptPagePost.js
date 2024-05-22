bda5670a395fce783ea335c111a610f1e9a0ad5d
Letícia Brudeck <leticiabrudeck_@hotmail.com>
2024-05-21 22:41:58 -0300

    Registro psicologo

--- src/main/resources/static/js/scriptPagePost.js
+++ src/main/resources/static/js/scriptPagePost.js
@@ -11,10 +11,6 @@ function getResponseApi() {
         });
 }
 
-// function addPost(post) {
-//     console.log(post["titulo"])
-
-// }
 
 function addPost(post) {
     // Seleciona a div onde as postagens serão inseridas
@@ -40,37 +36,4 @@ function addPost(post) {
 
     // Adiciona a nova postagem ao container de postagens
     postagensContainer.querySelector('.row').appendChild(postElement);
-}
-
-function redirectToPosts(){
-    window.location.href = '/pagePost.html';
-}
-
-
-function createPost() {
-    var postagem = {
-        "titulo": document.getElementById("postTitle").value,
-        "resumo": document.getElementById("postSummary").value,
-        "conteudo": document.getElementById("postContent").value,
-        "dataPostagem": document.getElementById("postDate").value,
-        "autor": document.getElementById("postAuthor").value
-    };
-
-    // console.log(postagem);
-
-    fetch('http://localhost:8080/cadastrar-postagem', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postagem)
-    })
-        .then(data => {
-            alert("Postagem criada com sucesso!");
-            window.location.href = "posts.html";
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-            alert("Ocorreu um erro ao criar a postagem.");
-        });
 }
